e9daacd4519264f4c10ad3bde0771e2d7bd6c3b6
baddin <baddin2016@gmail.com>
2018-09-06 22:28:17 +0100

    add some mock data and list them

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import './TodoList.css'
 import TodoItem from './TodoItem.js'
+import tasks from '../tasks'
 
 class TodoList extends Component {
   render() {
-
+  	const listItems = tasks.map(task => {
+  		
+  		return <TodoItem key={task.id} item={task.title}/>
+  	})
   return (
   		<div className="todo-list">
   			<ul className="list-group">
-  				<TodoItem item="test item"/>
+  				{listItems}
   			</ul>
   		</div>
   	)
---
+++ src/tasks.js
@@ -0,0 +1,18 @@
+const tasks = [
+	{
+		id: 1,
+		title: "task1"
+	},
+
+	{
+		id: 2,
+		title: "task2"
+	},
+
+	{
+		id: 3,
+		title: "task3"
+	}
+]
+
+export default tasks
