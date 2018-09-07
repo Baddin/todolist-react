c00140e7085c4ca561a118e9538224925601b3c6
baddin <baddin2016@gmail.com>
2018-09-07 12:46:19 +0100

    add a click event to 'add task button'

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import './TodoInput.css'
 
 class TodoInput extends Component {
+ 
+	onClick(){
+		if (this.textInput.value){
+  		console.log(this.textInput.value)
+  	}
+  	}
+
   render() {
+
 
   return (
    <div className='input-group'>
-          <input  className='form-control input-todo' type="text" placeholder="type a new task here"></input>
+          <input  ref={input => this.textInput = input} className='form-control input-todo' type="text" placeholder="type a new task here"></input>
            <span className='input-group-btn'>
-          	<button className='btn btn-default input-todo'>Add</button>
+          	<button onClick= {this.onClick.bind(this)} className='btn btn-default input-todo'>Add</button>
           </span>
         </div>
   	)
